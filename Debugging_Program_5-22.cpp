652d5f0afde6f114f1687cdfdcc358597382d5b7
Drot88 <Daniel.Rotenberg88@gmail.com>
2017-06-27 21:32:13 -0400

    Finished initial program layout, still need debugging

--- Debugging_Program_5-22.cpp
+++ Debugging_Program_5-22.cpp
@@ -5,16 +5,45 @@
 #include <iomanip>
 
 int main() {
-  string name;            // Student first name
+  std::string name;            // Student first name
 
   int count = 1,          // Student counter
       score,              // Individual score read in
       totalScore = 0;     // Total of a studnet's 2 scores
   double average;         // Average of a studnets 2 scores
 
-  cout << fixed << showpoint << setprecision(1);
-  cout << "Enter the first name of student " << count
+  std::cout << std::fixed << std::showpoint << std::setprecision(1);
+  std::cout << "Enter the first name of student " << count
        << " (or Q to quit): ";
+  std::cin >> name;
 
+  while (name != "Q" && name != "q") {
+    // Get and validate the first score
+    std::cout << "Enter score 1: ";
+    std::cin >> score;
+    if (score <= 0 || score >= 100) {
+      std::cout << "Score must be between 0 and 100. Please reenter: ";
+      std::cin >> score;   
+    }
+    totalScore += score;    // Add the first score onto the total
+
+    // Get and validate the second score
+    std::cout << "Enter score 2: ";
+    std::cin >> score;
+    if (score <= 0 || score >= 100) {
+      std::cout << "Score must be between 0 and 100. Please reenter: ";
+      std::cin >> score;
+    }
+    totalScore += score;    // Add the second score onto the total
+
+    // Calculate and print average
+    average = totalScore / 2;
+    std::cout << name << std::setw(6) << average << std::endl;
+
+    // Get the next student name
+    std::cout << "Enter the first name of student " << count++
+              << " (or Q to quit): ";
+    std::cin >> name;
+  }
   return 0;
 }
