7f1d878af0023c9a63a6fb89bc42fe04ded4f70b
Drot88 <Daniel.Rotenberg88@gmail.com>
2017-06-27 21:19:25 -0400

    Initial Commit

---
+++ Debugging_Program_5-22.cpp
@@ -0,0 +1,20 @@
+//This program attempts to average 2 test scores for each student in a class.
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+int main() {
+  string name;            // Student first name
+
+  int count = 1,          // Student counter
+      score,              // Individual score read in
+      totalScore = 0;     // Total of a studnet's 2 scores
+  double average;         // Average of a studnets 2 scores
+
+  cout << fixed << showpoint << setprecision(1);
+  cout << "Enter the first name of student " << count
+       << " (or Q to quit): ";
+
+  return 0;
+}
