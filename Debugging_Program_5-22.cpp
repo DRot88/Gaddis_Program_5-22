51a042ce201a5dc6d6bc3eb109cf0f0493a1ff90
Drot88 <Daniel.Rotenberg88@gmail.com>
2017-06-27 22:09:03 -0400

    Corrected the logic errors, program passes test cases

--- Debugging_Program_5-22.cpp
+++ Debugging_Program_5-22.cpp
@@ -9,8 +9,8 @@ int main() {
 
   int count = 1,          // Student counter
       score,              // Individual score read in
-      totalScore = 0;     // Total of a studnet's 2 scores
-  double average;         // Average of a studnets 2 scores
+      totalScore = 0;     // Total of a students's 2 scores
+  double average;         // Average of a students 2 scores
 
   std::cout << std::fixed << std::showpoint << std::setprecision(1);
   std::cout << "Enter the first name of student " << count
@@ -18,10 +18,11 @@ int main() {
   std::cin >> name;
 
   while (name != "Q" && name != "q") {
+    totalScore = 0.0;
     // Get and validate the first score
     std::cout << "Enter score 1: ";
     std::cin >> score;
-    if (score <= 0 || score >= 100) {
+    while (score < 0 || score > 100) {
       std::cout << "Score must be between 0 and 100. Please reenter: ";
       std::cin >> score;   
     }
@@ -30,18 +31,18 @@ int main() {
     // Get and validate the second score
     std::cout << "Enter score 2: ";
     std::cin >> score;
-    if (score <= 0 || score >= 100) {
+    while (score < 0 || score > 100) {
       std::cout << "Score must be between 0 and 100. Please reenter: ";
       std::cin >> score;
     }
     totalScore += score;    // Add the second score onto the total
 
     // Calculate and print average
-    average = totalScore / 2;
+    average = totalScore / 2.0;
     std::cout << name << std::setw(6) << average << std::endl;
 
     // Get the next student name
-    std::cout << "Enter the first name of student " << count++
+    std::cout << "Enter the first name of student " << ++count
               << " (or Q to quit): ";
     std::cin >> name;
   }
